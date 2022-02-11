98216968d3f537fd486c423c6a94ab9a18729415
Sandro Rochel <sandro.rochel@gmail.com>
2022-02-11 10:36:31 -0300

    Aula e exercícios de orientação a objeto

---
+++ orientacao_a_objeto/src/heranca/Mensalista.java
@@ -0,0 +1,19 @@
+package heranca;
+
+public class Mensalista extends Colaborador {
+    private double salarioMensal;
+
+    public Mensalista(String nome, String departamento, double salarioMensal) {
+        super(nome, departamento);
+        this.salarioMensal = salarioMensal;
+    }
+
+    public double salario() {
+        return this.salarioMensal;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s\nSalário Mensal: R$ %.2f", super.toString(), this.salario());
+    }
+}
